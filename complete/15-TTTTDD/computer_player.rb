b2052f9120dc5c26235bfd997ef85c8f03175f21
Brian Gates <core.safecrlf>
2015-06-15 01:53:35 -0400

    refactor computer player move

--- complete/15-TTTTDD/computer_player.rb
+++ complete/15-TTTTDD/computer_player.rb
@@ -6,23 +6,30 @@ class ComputerPlayer
   end
 
   def move(board)
-    if winning_move = board.winning_move_for(@marker)
-      [ winning_move[0], winning_move[1] ]
-    elsif winning_move = board.winning_move_for(opponent_marker)
-      [ winning_move[0], winning_move[1] ]
-    else
-      legal = false
-      until legal
-        row, column = rand(2), rand(2)
-        legal = board.legal_move?(row, column)
-      end
-      [row, column]
-    end
+    winning_move_for(board) || blocking_move_for(board) ||
+    random_legal_move_for(board)
   end
 
   private
 
     def opponent_marker
       @marker == 'X' ? 'O' : 'X'
+    end
+
+    def winning_move_for(board)
+      board.winning_move_for(@marker)
+    end
+
+    def blocking_move_for(board)
+      board.winning_move_for(opponent_marker)
+    end
+
+    def random_legal_move_for(board)
+      legal = false
+      until legal
+        row, column = rand(2), rand(2)
+        legal = board.legal_move?(row, column)
+      end
+      [row, column]
     end
 end
