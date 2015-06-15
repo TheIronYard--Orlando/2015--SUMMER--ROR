b27ddf8265dfbbaf637bbaeb3fe3cf9c1ddd32ea
Brian Gates <core.safecrlf>
2015-06-14 23:16:01 -0400

    ensure game starts w 2 players

--- complete/15-TTTTDD/game.rb
+++ complete/15-TTTTDD/game.rb
@@ -1,2 +1,5 @@
 class Game
+
+  def initialize(player_1_name, player_2_name)
+  end
 end
--- complete/15-TTTTDD/game_test.rb
+++ complete/15-TTTTDD/game_test.rb
@@ -7,4 +7,8 @@ class GameTest < MiniTest::Test
   def test_game_exists
     assert Game
   end
+
+  def test_game_starts_with_two_players
+    @game = Game.new('player_1', 'player_2')
+  end
 end
