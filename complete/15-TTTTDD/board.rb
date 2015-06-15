a6c980b404de31386767f399238dd766dd55942c
Brian Gates <core.safecrlf>
2015-06-14 22:48:21 -0400

    ensure board has collection of spaces

--- complete/15-TTTTDD/board.rb
+++ complete/15-TTTTDD/board.rb
@@ -1,2 +1,8 @@
 class Board
+  attr_reader :spaces
+
+  def initialize
+    @spaces = [] # it won't be an empty array, but this is the simplest way to get
+                 # the test to pass.
+  end
 end
--- complete/15-TTTTDD/board_test.rb
+++ complete/15-TTTTDD/board_test.rb
@@ -7,4 +7,18 @@ class BoardTest < MiniTest::Test
   def test_board_class_exists
     assert Board
   end
+
+  #def test_board_starts_with_3x3_grid_of_empty_spaces 
+  # @board = Board.new
+  # assert_equal 3, @board.grid.length
+  # assert_equal 3, @board.grid[0].length
+  # assert @board.all?{|row| row.all?{|space| space == ' ' }}
+  #end
+  # I decided the above test was doing too much, so I split it up
+
+  def test_board_starts_with_collection_of_spaces
+    @board = Board.new
+    assert @board.spaces.is_a?(Array)
+  end
+
 end
