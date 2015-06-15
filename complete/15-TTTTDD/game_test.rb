7723d2ef15e453c43b826fc27c93b9bf53de737a
Brian Gates <core.safecrlf>
2015-06-14 23:26:40 -0400

    refactor game test

--- complete/15-TTTTDD/game_test.rb
+++ complete/15-TTTTDD/game_test.rb
@@ -4,17 +4,19 @@ require './game'
 
 class GameTest < MiniTest::Test
 
+  def setup
+    @game = Game.new('player_1', 'player_2')
+  end
+
   def test_game_exists
     assert Game
   end
 
   def test_game_starts_with_two_players
-    @game = Game.new('player_1', 'player_2')
     assert @game
   end
 
   def test_new_game_has_new_board
-    @game = Game.new('player_1', 'player_2')
     assert @game.board.is_a?(Board)
   end
 
